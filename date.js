f5a662b30377dba872b905d6509bf52a5ab15b1d
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-10-01 23:13:18 -0500

    add dateToString

--- date.js
+++ date.js
@@ -21,3 +21,17 @@ $.substractDays = function (date, days) {
     date.setDate(date.getDate() - days);
     return date;
 }
+
+/**
+ * Devolver fecha en formato string YYYY-mm-dd
+ * @param {date} date Ejem: new Date()
+ * @returns {string}
+ *  */
+$.dateToString = function (date) {
+    if (date instanceof Date) {
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        const year = date.getFullYear();
+        return [year, month, day].join("-");
+    }
+}
