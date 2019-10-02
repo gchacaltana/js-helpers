5683739da884fa67ead8884234636aeebb53509e
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-10-01 23:15:49 -0500

    add getMonthNameByNum

--- date.js
+++ date.js
@@ -35,3 +35,27 @@ $.dateToString = function (date) {
         return [year, month, day].join("-");
     }
 }
+
+/**
+ * Devuelve nombre de mes de un número (1 al 12)
+ * @param {int} number
+ * @returns {string} Ejem: Septiembre 
+ * */
+$.getMonthNameByNum = function (number) {
+    const months = {
+        1: "Enero",
+        2: "Febrero",
+        3: "Marzo",
+        4: "Abril",
+        5: "Mayo",
+        6: "Junio",
+        7: "Julio",
+        8: "Agosto",
+        9: "Septiembre",
+        10: "Octubre",
+        11: "Noviembre",
+        12: "Diciembre"
+    };
+    const n = parseInt(number);
+    return (n >= 1 && n <= 12) ? months[n] : "Undefined";
+}
